52e7485dc2f7edf813f581a9ecaa16c66c026f7e
Kimberly Souravong <kimberly-souravong@uiowa.edu>
2020-03-21 04:22:37 -0500

    Ability to update and delete a row

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
@@ -27,16 +27,18 @@ class ksouravong_apps(db.Model):
         return "ID: {0} | App Name: {1} | Category: {2} | Category Rank: {3} | Free or Paid: {4} | Total Ratings: {5}".format(self.app_id, self.app_name, self.category, self.category_rank, self.app_type, self.total_ratings)
 
 class MobileAppForm(FlaskForm):
+    app_id = IntegerField('App ID:')
     app_name = StringField('App Name:', validators=[DataRequired()])
     category = StringField('Category:', validators=[DataRequired()])
-    category_rank = StringField('Category Ranking:', validators=[DataRequired()])
+    category_rank = IntegerField('Category Ranking:', validators=[DataRequired()])
     app_type = StringField('Free or Paid:', validators=[DataRequired()])
-    total_ratings = StringField('Total Ratings:', validators=[DataRequired()])
+    total_ratings = IntegerField('Total Ratings:', validators=[DataRequired()])
 
 @app.route('/')
 def index():
     all_apps = ksouravong_apps.query.all()
     return render_template('index.html', apps=all_apps, pageTitle='Mobile Apps')
+
 
 @app.route('/add_app', methods=['GET', 'POST'])
 def add_app():
@@ -49,6 +51,7 @@ def add_app():
 
     return render_template('add_app.html', form=form, pageTitle='Add New App')
 
+
 @app.route('/delete_app/<int:app_id>', methods=['GET', 'POST'])
 def delete_app(app_id):
     if request.method == 'POST':
@@ -58,6 +61,37 @@ def delete_app(app_id):
         return redirect("/")
     else:
         return redirect("/")
+
+
+@app.route('/app/<int:app_id>', methods=['GET', 'POST'])
+def get_app(app_id):
+    app = ksouravong_apps.query.get_or_404(app_id)
+    return render_template('app.html', form=app, pageTitle='App Details', legend="App Details")
+
+
+@app.route('/app/<int:app_id>/update', methods=['GET', 'POST'])
+def update_app(app_id):
+    app = ksouravong_apps.query.get_or_404(app_id)
+    form = MobileAppForm()
+
+    if form.validate_on_submit():
+        app.app_name = form.app_name.data
+        app.category = form.category.data
+        app.category_rank = form.category_rank.data
+        app.app_type = form.app_type.data
+        app.total_ratings = form.total_ratings.data
+        db.session.commit()
+        return redirect(url_for('get_app', app_id=app.app_id))
+   
+    form.app_id.data = app.app_id
+    form.app_name.data = app.app_name 
+    form.category.data = app.category  
+    form.category_rank.data = app.category_rank  
+    form.app_type.data = app.app_type 
+    form.total_ratings.data = app.total_ratings
+    return render_template('update_app.html', form=form, pageTitle='Update App', legend="Update An App")
+
+
 
 
 if __name__ == '__main__':
